8df6dafd10c6ac4e16d6bd8e04b8caf28db4cdc6
Guillaume Viger <fenekku@fenekku.com>
2024-05-07 12:57:40 -0400

    ui: [#855] add community membership request label type

--- invenio_requests/assets/semantic-ui/js/invenio_requests/contrib/labels/TypeLabel.js
+++ invenio_requests/assets/semantic-ui/js/invenio_requests/contrib/labels/TypeLabel.js
@@ -1,5 +1,6 @@
 // This file is part of InvenioRequests
 // Copyright (C) 2022 CERN.
+// Copyright (C) 2024 Northwestern University.
 //
 // Invenio RDM Records is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
